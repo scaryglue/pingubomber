2493e705879130715b28278f90e299a49ae092df
Hamza Ahmad <hamza.ahmad2612@gmail.com>
2021-05-18 18:01:11 +0200

    added some new menu elements

--- PinguBomber/Assets/Scripts/PlayerManager.cs
+++ PinguBomber/Assets/Scripts/PlayerManager.cs
@@ -11,6 +11,16 @@ public class PlayerManager : MonoBehaviour
 
     private List<PlayerInput> players = new List<PlayerInput>();
 
+    public GameObject Player1Join;
+    public GameObject Player2Join;
+
+    public GameObject Countdown3;
+    public GameObject Countdown2;
+    public GameObject Countdown1;
+    public GameObject CountdownGO;
+
+    public GameObject CountdownBackground;
+
     public void OnJoin(PlayerInput pi)
     {
         if(numPlayers == 0)
@@ -20,6 +30,7 @@ public class PlayerManager : MonoBehaviour
             pi.GetComponent<BombSpawner>().tilemap = thisTilemap;
             pi.GetComponent<PlayerController>().playerNumber = 1;
             pi.GetComponent<PlayerController>().tilemap = thisTilemap;
+            Player1Join.SetActive(false);
         }
         else if(numPlayers == 1)
         {
@@ -28,6 +39,7 @@ public class PlayerManager : MonoBehaviour
             pi.GetComponent<BombSpawner>().tilemap = thisTilemap;
             pi.GetComponent<PlayerController>().playerNumber = 2;
             pi.GetComponent<PlayerController>().tilemap = thisTilemap;
+            Player2Join.SetActive(false);
         }
         players.Add(pi);
         numPlayers++;
@@ -41,11 +53,36 @@ public class PlayerManager : MonoBehaviour
 
     IEnumerator waitTilBeginning()
     {
+        CountdownBackground.SetActive(true);
+        Countdown3.SetActive(true);
+
         yield return new WaitForSeconds(1f);
+
+        Countdown3.SetActive(false);
+
+        Countdown2.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        Countdown2.SetActive(false);
+
+        Countdown1.SetActive(true);
+        yield return new WaitForSeconds(1f);
+
+        Countdown1.SetActive(false);
+
+
+        CountdownGO.SetActive(true);
+        CountdownBackground.SetActive(false);
+
+
+
         foreach (PlayerInput player in players)
         {
             player.ActivateInput();
         }
+
+        yield return new WaitForSeconds(0.5f);
+        CountdownGO.SetActive(false);
     }
 
     public void DisableInputs()
