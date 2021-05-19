b9c50ea855ee9a36e1ce00a6a79f55b08f21228a
Hamza <hamza.ahmad2612@gmail.com>
2021-05-19 23:18:35 +0200

    added easter egg

---
+++ PinguBomber/Assets/Scripts/CheatTester.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheatTester : MonoBehaviour
+{
+    public AudioSource cell;
+
+    public AudioSource music;
+
+    public void ProsiebenCheat(string input) {
+        if(input.Equals("STOPBEINGMUSLIM")) {
+            music.Stop();
+            cell.Play();
+            StartCoroutine(waitMusic(cell, music));
+        }
+    }
+
+    IEnumerator waitMusic(AudioSource cell, AudioSource music) {
+        while(cell.isPlaying) {
+            yield return new WaitForSeconds(1f);
+        }
+
+        music.Play();
+    }
+}
